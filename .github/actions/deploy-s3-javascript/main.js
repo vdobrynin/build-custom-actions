5e4e9f1b0c8021559ec410836620cb334bc50a28
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-05-12 22:34:03 -0700

    added first custom js action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,9 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+function run() {
+    core.notice('Hello from my custom JavaScript Action!!!');
+}
+
+run();
