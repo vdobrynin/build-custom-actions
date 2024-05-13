dc4304c0ebec314225092bf58b0c4093e8cfa1e8
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-05-13 12:16:22 -0700

    update npm & checkout code

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello from my custom JavaScript Action!!!');
+    core.notice('Hello from my custom JavaScript Action!');
 }
 
 run();
