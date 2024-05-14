36a13d0f363928f7f30908059b2222411247bcdd
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-05-13 21:39:09 -0700

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,7 +12,9 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom JavaScript Action!');
-}
+    // core.notice('Hello from my custom JavaScript Action!');
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOutput('website-url', websiteUrl); // ::set-output
+} 
 
 run();
