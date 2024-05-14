a2d40e1c31d301eadbeb4801d1d038bebbd15c3a
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-05-13 17:06:07 -0700

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,15 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    // Get some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // Upload files 
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('Hello from my custom JavaScript Action!');
 }
 
